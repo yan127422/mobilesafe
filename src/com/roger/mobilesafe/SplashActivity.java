5af86486d2ebc395122d493042f2bfd412734928
yan127422 <yan127422@163.com>
2014-09-30 14:21:09 +0800

    .

--- src/com/roger/mobilesafe/SplashActivity.java
+++ src/com/roger/mobilesafe/SplashActivity.java
@@ -1,19 +1,34 @@
 package com.roger.mobilesafe;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * 引导界面
  */
 public class SplashActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
-
+    private TextView tv_version;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_splash);
+        tv_version = (TextView) findViewById(R.id.tv_splash_version);
+        tv_version.setText(getVersionName());
+    }
+
+
+    private String getVersionName(){
+        String version = "";
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(getPackageName(),0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return version;
     }
 }
