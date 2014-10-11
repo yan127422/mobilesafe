2788791441c8c8c39dae51c11f62c241c1bf5e9d
yan127422 <yan127422@163.com>
2014-10-11 09:13:08 +0800

    .

--- src/com/roger/mobilesafe/utils/MyConstants.java
+++ src/com/roger/mobilesafe/utils/MyConstants.java
@@ -5,4 +5,5 @@ package com.roger.mobilesafe.utils;
  */
 public class MyConstants {
     public static final String IS_AUTO_UPDATE = "isAutoUpdate";
+    public static final String SETUP_PWD = "setupPWD";//手机防盗密码
 }
